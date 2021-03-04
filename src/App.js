e7c3caa5cf0f34eff33114daa6f8bac3e1016434
alexangria1992 <alex040892@gmail.com>
2021-03-04 14:41:28 +1300

    Hero

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import React from 'react'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   )
 }
